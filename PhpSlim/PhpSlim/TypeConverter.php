a3c31d8b87865ea9c21e35239a112b2bfdf88ca1
Gregor Gramlich <gramlich@eosc.de>
2010-03-28 21:47:37 +0200

    improved rendering of floats

--- PhpSlim/PhpSlim/TypeConverter.php
+++ PhpSlim/PhpSlim/TypeConverter.php
@@ -119,17 +119,92 @@ class PhpSlim_TypeConverter
 
     public static function floatToString($value)
     {
-        $sign = ($value < 0)? '-': '';
-        $value = abs($value);
-        $int = floor($value);
-        $fract = 10.0 * ($value - $int);
-        $percent = substr((string) $fract, 2);
-        $fract = (int)((string)$fract);
-        $lotsOfSubsequentZeros = strpos($percent, '00000000000');
-        if (false !== $lotsOfSubsequentZeros) {
-            $percent = substr($percent, 0, $lotsOfSubsequentZeros);
+        $value = sprintf('%1.16F', $value);
+        $value = self::stripTrailingNines($value);
+        $value = self::stripTrailingZeros($value);
+        if ((abs($value) < 1e-4) && (0 != $value)) {
+            return self::scientificValue($value);
         }
-        return $sign . sprintf('%01d.%01d%s', $int, $fract, $percent);
+        if ((abs($value) >= 1e7)) {
+            return self::scientificValue($value);
+        }
+        return $value;
+    }
+
+    private static function stripTrailingZeros($value)
+    {
+        if (!self::hasLotsOfZeros($value) && (!self::hasTrailingZero($value))) {
+            return $value;
+        }
+        if (!self::hasTrailingZero($value)) {
+            $value = substr($value, 0, -5);
+        }
+        while (self::hasTrailingZero($value) && (substr($value, -2) !== '.0')) {
+            $value = substr($value, 0, -1);
+        }
+        return $value;
+    }
+
+    private static function stripTrailingNines($value)
+    {
+        if (!self::hasLotsOfNines($value)) {
+            return $value;
+        }
+        if (!self::hasTrailingNine($value)) {
+            $value = substr($value, 0, -4);
+        }
+        while (self::hasTrailingNine($value)) {
+            $value = substr($value, 0, -1);
+        }
+        $precision = strlen($value) - strpos($value, '.') - 1;
+        $digit = ((float) ('1E-' . $precision));
+        if ($value > 0) {
+            $value = $value + $digit;
+        } else {
+            $value = $value - $digit;
+        }
+        return (string) $value;
+    }
+
+    private static function hasLotsOfZeros($value)
+    {
+        return false !== strpos($value, '000000000');
+    }
+
+    private static function hasTrailingZero($value)
+    {
+        return substr($value, -1) === '0';
+    }
+
+    private static function hasLotsOfNines($value)
+    {
+        return false !== strpos($value, '999999999');
+    }
+
+    private static function hasTrailingNine($value)
+    {
+        return substr($value, -1) === '9';
+    }
+
+    private static function scientificValue($value)
+    {
+        $precision = max(1, self::getScientificPrecision($value));
+        return sprintf('%.' . $precision . 'E', $value);
+    }
+
+    private static function getScientificPrecision($value)
+    {
+        $digits = substr($value, strpos($value, '.') + 1);
+        if (abs($value) < 1) {
+            while ('0' === substr($digits, 0, 1)) {
+                $digits = substr($digits, 1);
+            }
+        } else {
+            while (is_numeric(substr($digits, 0, 1))) {
+                $digits = substr($digits, 1);
+            }
+        }
+        return strlen($digits) -1;
     }
 
     public static function boolToString($value)
