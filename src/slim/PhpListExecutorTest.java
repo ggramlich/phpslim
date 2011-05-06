014db6637fc3f32ab7eee6e438655368b47957b3
Gregor Gramlich <gramlich@eosc.de>
2011-05-06 22:52:22 +0200

    Have the ListExecutorTest compare results on a String basis

--- src/slim/PhpListExecutorTest.java
+++ src/slim/PhpListExecutorTest.java
@@ -2,8 +2,13 @@
 // Released under the terms of the CPL Common Public License version 1.0.
 package slim;
 
+import static org.junit.Assert.assertEquals;
 import static slim.TestSuite.getTestIncludePath;
 import static util.ListUtility.list;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -11,12 +16,13 @@ import org.junit.Test;
 
 import fitnesse.slim.ListExecutor;
 import fitnesse.slim.ListExecutorTestBase;
+import fitnesse.slim.SlimClient;
 import fitnesse.slim.SlimFactory;
 
 public class PhpListExecutorTest extends ListExecutorTestBase {
 
   private static SlimFactory slimFactory;
-  
+
   @BeforeClass
   public static void setUpClass() {
     slimFactory = new PhpSlimFactory(getTestIncludePath());
@@ -35,6 +41,31 @@ public class PhpListExecutorTest extends ListExecutorTestBase {
   @Override
   protected String getTestClassPath() {
     return "testModule";
+  }
+
+  @Override
+  protected void respondsWith(List<Object> expected) {
+    expectedResults.addAll(expected);
+    List<Object> result = executor.execute(statements);
+    Map<String, Object> expectedMap = SlimClient.resultToMap(expectedResults);
+    Map<String, Object> resultMap = removeProxies(SlimClient
+        .resultToMap(result));
+    assertEquals(expectedMap, resultMap);
+  }
+
+  private Map<String, Object> removeProxies(Map<String, Object> resultMap) {
+    Map<String, Object> map = new HashMap<String, Object>();
+    for (String string : resultMap.keySet()) {
+      map.put(string, removeProxy(resultMap.get(string)));
+    }
+    return map;
+  }
+
+  private Object removeProxy(Object object) {
+    if (object == null || object instanceof List) {
+      return object;
+    }
+    return object.toString();
   }
 
   @Override
