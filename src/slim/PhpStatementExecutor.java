ddc49e762a3eccf9136f09a26c3a3c88dfa5b7d3
Gregor Gramlich <gramlich@eosc.de>
2011-05-06 15:44:57 +0200

    Added callAndAssign method

--- src/slim/PhpStatementExecutor.java
+++ src/slim/PhpStatementExecutor.java
@@ -5,15 +5,21 @@ import fitnesse.slim.Jsr223StatementExecutor;
 
 public class PhpStatementExecutor extends Jsr223StatementExecutor {
 
-  public PhpStatementExecutor(Jsr223Bridge bridge) throws Exception
-  {
+  public PhpStatementExecutor(Jsr223Bridge bridge) throws Exception {
     super(bridge);
   }
-  
+
   @Override
   public void setVariable(String name, Object value) {
     // Packing the value into a single element array, because a null value
     // caused the proxy call to hang.
-    callMethod("setSymbol", new Object[] {name, new Object[] {value}});
+    callMethod("setSymbol", new Object[] { name, new Object[] { value } });
+  }
+
+  @Override
+  public Object callAndAssign(String variable, String instanceName,
+      String methodName, Object[] args) {
+    return callMethod("callAndAssign", new Object[] { variable, instanceName,
+        methodName, args });
   }
 }
